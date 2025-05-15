afde7624e7524dbf595c2f7e4f366febf310b23b
Dheimy <jheimyeng@hotmail.com>
2025-05-15 00:24:36 -0300

    fix: ajust terraform

--- main.tf
+++ main.tf
@@ -21,22 +21,12 @@ resource "aws_security_group" "rds" {
   tags        = var.tags
 
   ingress {
-    description     = "Allow access from allowed SGs"    
+    description     = "Allow access from allowed SGs"
     from_port       = var.db_port
     to_port         = var.db_port
     protocol        = "tcp"
     security_groups = var.allowed_security_groups
-  }
-
-    dynamic "ingress" {
-    for_each = var.allowed_cidr_blocks
-    content {
-      description = "Allow access from CIDR block"
-      from_port   = var.db_port
-      to_port     = var.db_port
-      protocol    = "tcp"
-      cidr_blocks = [ingress.value]
-    }
+    cidr_blocks     = var.allowed_cidr_blocks
   }
 
   egress {
